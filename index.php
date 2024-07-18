17d40a5be976188d53caee3b158a61c9f7f3d497
sapsco <seyyed.amin.8809@gmail.com>
2024-07-18 15:02:36 -0700

    found bug notes

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
 require "functions.php";
 require "Database.php";
+require "router.php";
 
-//require "router.php";
 
 //connect to our database Mysql
 
 $config = require "config.php";
 
-
-
 $db = new Database($config['database']);
-
-$id = $_GET["id"];
-
-$query = "SELECT * FROM posts where id = ?";
-
-
-$posts = $db->query($query, [$id])->fetch();
-
-dd($posts);
 
--- router.php
+++ router.php
@@ -7,6 +7,7 @@ $url = parse_url($_SERVER['REQUEST_URI'])["path"];
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
     '/contact' => 'controllers/contact.php',
 ];
 
@@ -15,11 +16,11 @@ function routeToController($url, $routes){
     if (array_key_exists($url, $routes)) {
         require $routes[$url];
     }else {
-        abort(404);
+        abort();
     }
 }
 
-function abort($code) {
+function abort($code = 404) {
     http_response_code($code);
     require "view/{$code}.php";
     die();
