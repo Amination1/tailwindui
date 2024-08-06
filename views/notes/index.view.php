44095a73f7575c8c2c5174c5009afe361635a378
sapsco <seyyed.amin.8809@gmail.com>
2024-08-07 01:29:16 +0330

    replace laracasts files again

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -1,6 +1,6 @@
-<?php require base_path("view/partials/head.php"); ?>
-<?php require base_path("view/partials/nav.php"); ?>
-<?php require base_path("view/partials/banner.php"); ?>
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -13,10 +13,11 @@
                 </li>
             <?php endforeach; ?>
         </ul>
+
         <p class="mt-6">
-            <a href="/notes/create" class="text-blue-500 hover:underline">Create new note</a>
+            <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
         </p>
     </div>
 </main>
 
-<?php require base_path("view/partials/footer.php"); ?>
+<?php require base_path('views/partials/footer.php') ?>
